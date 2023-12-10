8c9337a55e5ad8963b5721a767cb34e1df0e7cc6
Alec Condry <aleccondry@gmail.com>
2023-12-10 15:57:09 -0500

    Finished unparallelized logistic regression

---
+++ src/LogisticRegression.py
@@ -0,0 +1,140 @@
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+import pandas as pd
+from LogisticRegressionModel import LogisticRegressionModel
+import random
+
+
+def write_beta(output, beta_to_save, feature_names):
+    with open(output, 'w') as f:
+        for val, label in zip(beta_to_save, feature_names):
+            f.write('(%s,%f)\n' % (label, val))
+
+
+def read_features_pd(input_file: str) -> pd.DataFrame:
+    data = pd.read_csv(input_file)
+    data.drop(columns=['Diabetes_binary'], inplace=True)
+    return data
+
+
+def read_features_np(input_file: str) -> np.array:
+    return np.array(read_features_pd(input_file))
+
+
+def read_labels_pd(input_file: str) -> pd.DataFrame:
+    return pd.read_csv(input_file)['Diabetes_binary']
+
+
+def read_labels_np(input_file: str) -> np.array:
+    return np.array(read_labels_pd(input_file))
+
+
+def get_all_features(data: pd.DataFrame) -> list:
+    return list(data.columns)
+
+
+def random_oversample(features, labels):
+    pos_feats = [x for i, x in enumerate(features) if labels[i] == 1]
+    neg_feats = [x for i, x in enumerate(features) if labels[i] == 0]
+    num_pos = len(pos_feats)
+    num_neg = len(neg_feats)
+    num_less = abs(num_pos - num_neg)
+    choice_arr = pos_feats if num_pos < num_neg else neg_feats
+    choice_label = 1 if num_pos < num_neg else 0
+    for i in range(num_less):
+        features.append(random.choice(choice_arr))
+        labels.append(choice_label)
+    return np.array(features), np.array(labels)
+
+
+# https://stackoverflow.com/questions/4601373/better-way-to-shuffle-two-numpy-arrays-in-unison
+def unison_shuffled_copies(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
+
+
+def create_data_plots(k, data, label=""):
+    plt.plot([i for i in range(1, k)], data, label=label)
+    plt.xlabel("Number of Iterations")
+    plt.ylabel(label)
+    plt.title(f'{label} vs Number of Iterations')
+    plt.show()
+
+
+def create_metric_plots(k, metrics, label=""):
+    iters = [x for x in range(1, k)]
+    accs = [x[0] for x in metrics]
+    pres = [x[1] for x in metrics]
+    recs = [x[2] for x in metrics]
+    plt.plot(iters, accs, label="Accuracy")
+    plt.plot(iters, pres, label="Precision")
+    plt.plot(iters, recs, label="Recall")
+    plt.legend(loc='upper right')
+    plt.xlabel("Number of Iterations")
+    plt.ylabel("Metric Value")
+    plt.title(f'{label} Metrics vs Number of Iterations')
+    plt.show()
+
+
+if __name__ == "__main__":
+    # Set up argument parser and associated flags
+    parser = argparse.ArgumentParser(description="Logistic Regression.",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--data', default='../data/Clean_data.csv',
+                        help='Input file containing all features and labels, used to train a logistic model')
+    parser.add_argument('--beta', default='beta', help='File where beta is stored')
+    parser.add_argument('--split', type=float, default=0.8,
+                        help='Test/Training split. Percentage of data to be used for training')
+    parser.add_argument('--lam', type=float, default=0.0, help="Regularization parameter λ")
+    parser.add_argument('--max_iter', type=int, default=10, help='Maximum number of iterations')
+    parser.add_argument('--eps', type=float, default=0.1,
+                        help='ε-tolerance. If the l2_norm gradient is smaller than ε, gradient descent terminates.')
+
+    args = parser.parse_args()
+
+    data_path = args.data
+    if data_path is None:
+        print("Please provide a valid data path using the --data flag")
+        exit(0)
+
+    print("Reading data from", data_path)
+    features_np = read_features_np(data_path)
+    features_pd = read_features_pd(data_path)
+    labels_np = read_labels_np(data_path)
+    labels_pd = read_labels_pd(data_path)
+    print('Read', len(labels_np), 'data points with', len(features_np[0]), 'features in total.')
+
+    print("Balancing dataset using random oversampling...")
+    features_np, labels_np = random_oversample(list(features_np), list(labels_np))
+    features_np, labels_np = unison_shuffled_copies(features_np, labels_np)
+    print("Oversampling complete")
+
+    feature_labels = get_all_features(features_pd)
+    model = LogisticRegressionModel(num_features=len(feature_labels))
+    TRAIN_SPLIT = args.split
+    END_IDX = int(len(labels_np) * TRAIN_SPLIT)
+
+    labels_np[labels_np == 0] = -1
+    x_train = features_np[0:END_IDX]
+    y_train = labels_np[0:END_IDX]
+    x_test = features_np[END_IDX:]
+    y_test = labels_np[END_IDX:]
+    assert ((len(x_train) + len(x_test)) == (len(y_train) + len(y_test)) == len(features_np))
+
+    print('Training on data from', args.data,
+          'with: λ = %f, ε = %f, max iter= %d:' % (args.lam, args.eps, args.max_iter))
+    t, k, losses, grad_norms, metrics_t, metrics_v, beta = model.train(x_train, y_train, x_test, y_test,
+                                                                       lam=args.lam,
+                                                                       eps=args.eps,
+                                                                       max_iter=args.max_iter)
+    print('Unparallelized logistic regression ran for', k, 'iterations. Converged', grad_norms[-1] < args.eps)
+    print("Saving trained β in", args.beta)
+    write_beta(args.beta, beta, feature_labels)
+
+    print("Creating plots of results...")
+    create_data_plots(k, losses, label="Loss")
+    create_data_plots(k, grad_norms, label='GradNorm')
+    create_metric_plots(k, metrics_t, label="Train")
+    create_metric_plots(k, metrics_v, label="Test")
---
+++ src/LogisticRegressionModel.py
@@ -0,0 +1,101 @@
+import numpy as np
+import time
+
+
+class LogisticRegressionModel:
+    def __init__(self, num_features, learning_rate=1e-5):
+        self.num_features = num_features
+        self.alpha = learning_rate
+        self.weights = np.zeros(num_features + 1)
+
+    def get_features(self, x):
+        return np.append([1], x)
+
+    def get_weights(self):
+        return self.weights
+
+    def logisticLoss(self, beta, x, y) -> float:
+        return np.log(1. + np.e ** (-y * np.dot(beta, self.get_features(x))))
+
+    def gradLogisticLoss(self, beta, x, y) -> np.array:
+        return -1. * y * x / (1. + np.e ** (y * np.dot(beta, x)))
+
+    def totalLoss(self, x_train, y_train, beta, lam=0.0) -> float:
+        return sum([self.logisticLoss(beta, x, y) for x, y in zip(x_train, y_train)]) + lam * np.dot(beta, beta)
+
+    def gradTotalLoss(self, x_train, y_train, beta, lam=0.0) -> np.array:
+        gradLogisticLosses = np.sum(np.array([self.gradLogisticLoss(beta, self.get_features(x), y)
+                                              for x, y
+                                              in zip(x_train, y_train)]), axis=0)
+        regularization = 2 * lam * beta
+        return gradLogisticLosses + regularization
+
+    def lineSearch(self, fun, x, grad, a=0.2, b=0.6):
+        t = 1e-5
+        fx = fun(x)
+        gradNormSq = np.dot(grad, grad)
+        while fun(x - t * grad) > fx - a * t * gradNormSq:
+            t = b * t
+        return t
+
+    def basicMetrics(self, x_data, y_data, beta):
+        for i in y_data:
+            try:
+                int(i)
+            except:
+                print(i)
+        pairs = ((int(np.sign(np.dot(beta, self.get_features(x)))), int(y)) for (x, y) in zip(x_data, y_data))
+        new_pairs = [(pred_label, pred_label * true_label) for (pred_label, true_label) in pairs]
+
+        TP = 1. * new_pairs.count((1, 1)) + 1
+        FP = 1. * new_pairs.count((1, -1)) + 1
+        TN = 1. * new_pairs.count((-1, 1)) + 1
+        FN = 1. * new_pairs.count((-1, -1)) + 1
+        P = TP + FP
+        N = TN + FN
+        return P, N, TP, FP, TN, FN
+
+    def metrics(self, P, N, TP, FP, TN, FN):
+        acc = (TP + TN) / (P + N)
+        pre = TP / (TP + FP)
+        rec = TP / (TP + FN)
+        return acc, pre, rec
+
+    def get_metrics(self, x_data, y_data, beta):
+        P, N, TP, FP, TN, FN = self.basicMetrics(x_data, y_data, beta)
+        return self.metrics(P, N, TP, FP, TN, FN)
+
+    def train(self, x_train, y_train, x_test, y_test, lam=1, eps=1e-3, max_iter=1000):
+        k = 1
+        grad_norm = 2 * eps
+        start = time.time()
+        losses, grad_norms, metrics_t, metrics_v = ([] for i in range(4))
+        while k < max_iter and grad_norm > eps:
+            grad = self.gradTotalLoss(x_train, y_train, self.weights, lam)
+            fun = lambda x: self.totalLoss(x_train, y_train, x, lam)
+            gamma = 1e-7
+            # gamma = self.lineSearch(fun, self.weights, grad)
+            self.weights -= gamma * grad
+            obj = fun(self.weights)
+            grad_norm = np.sqrt(np.dot(grad, grad))
+
+            # Evaluate training and test metrics
+            acc_t, pre_t, rec_t = self.get_metrics(x_train, y_train, self.weights)
+            acc_v, pre_v, rec_v = self.get_metrics(x_test, y_test, self.weights)
+
+            # Store data on current iteration
+            losses.append(obj)
+            grad_norms.append(grad_norm)
+            metrics_t.append((acc_t, pre_t, rec_t))
+            metrics_v.append((acc_v, pre_v, rec_v))
+
+            # Print results from current iteration
+            print(f'{k}: t={"{:.5f}".format(time.time() - start)}\t'
+                  f'L(β_k)={"{:.5f}".format(obj)}\t'
+                  f'||∇L(β_k)||_2={"{:.5f}".format(grad_norm)}\t'
+                  f'γ={"{:.5E}".format(gamma)}\t'
+                  f'train acc={"{:.5}".format(acc_t)}\t'
+                  f'test acc={"{:.5}".format(acc_v)}')
+            k += 1
+        return time.time() - start, k, losses, grad_norms, metrics_t, metrics_v, self.weights
+
